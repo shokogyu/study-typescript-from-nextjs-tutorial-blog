f46e09f4deae2d65f76ef2b25881d3360152de0e
Shoko Okamoto <mffnoissm23@gmail.com>
2023-11-27 17:55:16 +0900

    ♻️ dateファイルのTypeScript化

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,11 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO, format } from "date-fns";
+import { FC } from "react";
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-}
+type Props = { dateString: string };
+
+const Date: FC<Props> = ({ dateString }) => {
+    const date = parseISO(dateString);
+    return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+};
+
+export default Date;
